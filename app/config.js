c7755284a31e65b8d66ca9e0baf6db15d3a88ab4
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-04-30 15:19:46 -0400

    switch from traceur compiler to babel. Babel can compile to the system module format

--- app/config.js
+++ app/config.js
@@ -1,6 +1,11 @@
 System.config({
   "baseURL": "/",
-  "transpiler": "traceur",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
@@ -11,13 +16,17 @@ System.config({
 System.config({
   "map": {
     "angular": "npm:angular@1.4.0-rc.1",
+    "babel": "npm:babel-core@5.1.13",
+    "babel-runtime": "npm:babel-runtime@5.1.13",
+    "core-js": "npm:core-js@0.9.4",
     "lodash": "npm:lodash@3.7.0",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:angular@1.4.0-rc.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.4": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:lodash@3.7.0": {
