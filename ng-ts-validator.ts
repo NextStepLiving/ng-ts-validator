ca25f1694886d37ba11a748d30fe105e6548d3ad
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-05-15 15:36:45 -0400

    Re-exporting ts-validator objects from ng-ts-validator for convenience

--- ng-ts-validator.ts
+++ ng-ts-validator.ts
@@ -1,2 +1,8 @@
 import 'app/main';
-export { BaseModel } from 'app/main';
+export { BaseModel } from './app/libs/ts-validator/ts-validator';
+export { IValidatorObject } from './app/libs/ts-validator/ts-validator';
+
+export { notEmpty } from './app/libs/ts-validator/ts-validator';
+export { modelProp } from './app/libs/ts-validator/ts-validator';
+export { min } from './app/libs/ts-validator/ts-validator';
+export { max } from './app/libs/ts-validator/ts-validator';
