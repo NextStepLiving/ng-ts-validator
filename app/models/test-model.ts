fd96a6c5bbd828a580585672aa4dc2d830596b57
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-05-15 15:25:40 -0400

    was showing all the fields errors on the first error, now switched it to just when the form field errors, unless submit

--- app/models/test-model.ts
+++ app/models/test-model.ts
@@ -1,10 +1,16 @@
-import { BaseModel, modelProp, notEmpty, min} from '../libs/ts-validator/ts-validator';
+import { BaseModel, modelProp, notEmpty, min, max} from '../libs/ts-validator/ts-validator';
 
 export class TestModel extends BaseModel {
 
     @modelProp
     @notEmpty('TestProperty can not be empty')
-    @min({min: 10, message: 'test property must be greater than 10'})
+    @min({min: 11, message: 'test property must be greater than 10'})
     public testProperty: string;
+
+
+    @modelProp
+    @notEmpty('TestProperty2 can not be empty')
+    @max({max: 9, message: 'test property 2 must be less than 10'})
+    public testProperty2: number;
 
 }
