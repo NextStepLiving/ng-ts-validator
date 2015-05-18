735ad2e4946f03e02d212a714ac3fa76d5ca89bd
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-05-18 12:57:54 -0400

    exporting validatorFactory for conveneince, and updating ts-validator

--- ng-ts-validator.ts
+++ ng-ts-validator.ts
@@ -2,6 +2,7 @@ import './app/main';
 export { BaseModel } from './app/libs/ts-validator/ts-validator';
 export { IConfig } from './app/libs/ts-validator/ts-validator';
 export { IValidatorObject } from './app/libs/ts-validator/ts-validator';
+export { validatorFactory } from './app/libs/ts-validator/ts-validator';
 
 export { notEmpty } from './app/libs/ts-validator/ts-validator';
 export { modelProp } from './app/libs/ts-validator/ts-validator';
