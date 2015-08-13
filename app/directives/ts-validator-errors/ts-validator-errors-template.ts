52170618b9a565eee96cd704a6cd7617ca8da16b
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-08-12 20:33:12 -0400

    updating ts-validator and changing the ts-validator-errors template to properly display errors

--- app/directives/ts-validator-errors/ts-validator-errors-template.ts
+++ app/directives/ts-validator-errors/ts-validator-errors-template.ts
@@ -1,7 +1,8 @@
 export let tsValidatorErrorsTemplate = function () {
     return [
         '<div ng-show="(vm.forForm[vm.forField].$dirty || vm.forForm.$submitted) && vm.forForm.$invalid" class="ts-validator-error-for-{{vm.forField}}">',
-        '<div ng-repeat="(errorName, errorMessage) in vm.forModel._errorMessages[vm.forField]" ng-show="vm.forError[errorName] == true" class="ts-validator-error ts-validator-error-for-{{errorName}}">',
+        // '<div ng-repeat="(errorName, errorMessage) in vm.forModel._errorMessages[vm.forField]" ng-show="vm.forError[errorName] == true" class="ts-validator-error ts-validator-error-for-{{errorName}}">',
+        '<div ng-repeat="(errorName, errorMessage) in vm.forModel._errorMessages[vm.forField]" ng-show="vm.forForm[vm.forField].$error[errorName]" class="ts-validator-error ts-validator-error-for-{{errorName}}">',
         '{{errorMessage}}',
         '</div>',
         '</div>'
