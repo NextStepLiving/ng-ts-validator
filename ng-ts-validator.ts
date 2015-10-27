801b31190d43a67bfed562e847a916c30557a4ae
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-10-27 18:27:20 -0400

    Added customValidator decorator

--- ng-ts-validator.ts
+++ ng-ts-validator.ts
@@ -6,6 +6,7 @@ export { validatorFactory } from './app/libs/ts-validator/ts-validator';
 
 export { notEmpty } from './app/libs/ts-validator/ts-validator';
 export { modelProp } from './app/libs/ts-validator/ts-validator';
+export { jsonIgnore } from './app/libs/ts-validator/ts-validator';
 export { min } from './app/libs/ts-validator/ts-validator';
 export { max } from './app/libs/ts-validator/ts-validator';
 export { pattern } from './app/libs/ts-validator/ts-validator';
@@ -13,3 +14,4 @@ export { swaggerDef } from './app/libs/ts-validator/ts-validator';
 export { minLength } from './app/libs/ts-validator/ts-validator';
 export { maxLength } from './app/libs/ts-validator/ts-validator';
 export { allowedValues } from './app/libs/ts-validator/ts-validator';
+export { customValidator } from './app/libs/ts-validator/ts-validator';
